da2d318d68326c82ffdeb85291d8632d3ee32f84
TcardLab <tcardlab@gmail.com>
2019-08-17 23:10:11 -0400

    Init vuex

---
+++ store/index.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    boarder: 10, // 3
+    // tree
+    x1: 25, // 25
+    x2: 15, // 15
+    yGroup: 36, // 35
+    yInner: 12, // 10
+    vueblank: true,
+    vuebraille: true,
+    // Branches
+    width: 2.5,
+    gap: 1.5,
+    vuepath: true,
+    // letters
+    size: 8,
+    vuealph: false,
+    vuelabel: true,
+    display: 0 // 0(first 12),1(dot3),2(dot3&6),3(all)
+  })
