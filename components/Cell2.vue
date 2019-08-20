9ec77f7534501af52d59599098919b767cb66e16
TcardLab <tcardlab@gmail.com>
2019-08-20 04:43:16 -0400

    Finished condensing All Braille

--- components/Cell2.vue
+++ components/Cell2.vue
@@ -1,7 +1,7 @@
 <!--Renders indevidual Braille Cells-->
 <template>
   <svg :x="dx" :y="dy" style="overflow:visible;"
-    v-if="_$.display < 4 && _$.vuealph == 0">
+    v-if="_$.vuealph == 0">
     <circle
       v-for="(dot, i) in BinaryArray(for1, for2)"
       :key="(dot, i)"
@@ -12,22 +12,46 @@
       :stroke="_$.vueblank ? 'black' : 'none'"
       r="0.75mm"
     />
-    <g v-html="rawhtml"></g>
+    <g v-if="_$.display > 3" v-html="Iterate(for1, for2)"></g>
   </svg>
 </template>
 
 <script>
+// html of each dot in iterCell
+const Dot = function(dot, i, dx, stroke, dy) {
+  const fill = ['none', 'black'][dot]
+  const cx = i > 2 ? 3.25 : 0.75
+  const cy = (i % 3) * 2.5 + 0.75
+  dy = typeof dy === 'undefined' ? 0 : dy
+  return `<circle 
+            r="0.75mm" 
+            fill=${fill}
+            stroke=${stroke}
+            cx="${cx + dx}mm"
+            cy="${cy + dy}mm"
+          />`
+}
+
+// Recreation of Cell as components cannot be passed through v-html
+const iterCell = function(arr, dx, stroke, dy) {
+  let rawhtml = ''
+  let i
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] in [0, 1]) {
+      rawhtml += Dot(arr[i], i, dx, stroke, dy)
+    }
+  }
+  return rawhtml
+}
+
 export default {
   props: ['for1', 'for2', 'dx', 'dy', 'rawhtml'],
   methods: {
     // Top-2-Bottom
-    BinaryArray: function(for1, for2) {
-      if (this._$.display > 3) {
-        /* eslint-disable no-console */
-        console.log('tree cant take _$.display>3')
-      }
-      // NOTE: this controles bottom dots for non-all _$.displays
-      const [dot3, dot6] = [[0, 0], [0, 0], [1, 0], [1, 1]][this._$.display]
+    BinaryArray: function(for1, for2, display) {
+      //Note: 'Binarray' is called in 'Iterate', hence the 'display' parameter.
+      display = typeof display === 'undefined' ? this._$.display : display
+      const [dot3, dot6] = [[0, 0], [0, 0], [1, 0], [1, 1], [0, 0], [0, 0]][display]
       if (typeof for1 === 'undefined') {
         // First cell
         const cell1 = [0, 0, dot3, 0, 0, dot6]
@@ -49,6 +73,48 @@ export default {
           [dot6]
         )
         return lst // 2nd branch cells
+      }
+    },
+    Iterate: function(for1, for2) {
+      /* eslint-disable no-unused-vars, no-var, prefer-const */
+      // _$.display == 5 add dots bellow letter
+      const stroke = this._$.vueblank ? 'black' : 'none'
+      var cell0 = ['', '', 0, '', '', 0]
+      var cell1 = ['', '', 1, '', '', 0]
+      var cell2 = ['', '', 1, '', '', 1]
+      // _$.display == 4 add whole cell
+      if (+this._$.display === 4) {
+        cell0 = this.BinaryArray(for1, for2, 1)
+        cell1 = this.BinaryArray(for1, for2, 2)
+        cell2 = this.BinaryArray(for1, for2, 3)
+      }
+      if (typeof for1 === 'undefined') {
+        // First cell
+        const dx = +this._$.gap + +this._$.size + 4 // column displacement
+        return iterCell(cell1, -dx, stroke) + iterCell(cell2, -dx / 2, stroke)
+      } else if (typeof for2 === 'undefined') {
+        // 1st branch cells
+        const x = +this._$.x1 + 2 // 2nd branch location
+        const dx = +this._$.gap + +this._$.size - 3 // column displacement
+        const dy = +this._$.display === 4 ? 11 : 17
+        return (
+          iterCell(cell2, -dx + 2.4, stroke, dy) +
+          // iterCell(cell0, -dx * 1.5 - 1, stroke, 8) +
+          iterCell(cell1, -dx * 2 + 2, stroke, dy)
+        )
+      } else {
+        // 2nd branch cells
+        const x = +this._$.x1 + +this._$.x2 // 2nd branch location
+        const dx = +this._$.gap + +this._$.size + 1.1 // column displacement
+        if (+this._$.display === 5) {
+          return (
+            iterCell(cell0, dx, stroke) +
+            iterCell(cell1, dx * 2, stroke) +
+            iterCell(cell2, dx * 3, stroke)
+          )
+        } else {
+          return iterCell(cell1, dx, stroke) + iterCell(cell2, dx * 2, stroke)
+        }
       }
     }
   }
