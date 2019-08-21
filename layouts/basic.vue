c7fbd7b176e48385d442a02c70a12e085368bf0e
TcardLab <tcardlab@gmail.com>
2019-08-21 16:16:05 -0400

    successful grid system layout
    rough draft nav-bar

---
+++ layouts/basic.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
+    <!-- larger than md = 2-8-2 // md and below = 12(fullscreen) -->
+    <b-row class="justify-content-center">
+      <b-col col md="2" class="d-none d-md-table-cell">1 of 3</b-col>
+      <b-col sm="12" md="8">
+        <nuxt/> 
+      </b-col>
+      <b-col col md="2" class="d-none d-md-table-cell">3 of 3</b-col>
+    </b-row>
+
+      
+  </div>
+</template>
+
+<style>
+.container {
+  height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
       <input v-model="_$.display" type="range" min="0" max="5" />
       <span>{{ ["#'s", 'A-J', 'K-T', 'U-Z', 'All', 'All2'][_$.display] }}</span>
     </div>
+
   </div>
 </template>
 
@@ -14,6 +15,7 @@
 import BrailleTree from '~/components/BrailleTree.vue'
 
 export default {
+  layout: 'basic',
   components: {
     BrailleTree,
   },
