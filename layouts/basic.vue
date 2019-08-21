fbbf9cb64bd17b47929502d19c73ab029134e08b
TcardLab <tcardlab@gmail.com>
2019-08-21 17:14:11 -0400

    adjust containers so navbar reaches edges

--- layouts/basic.vue
+++ layouts/basic.vue
@@ -13,22 +13,25 @@
 
       </b-collapse>
     </b-navbar>
-
-    <!-- larger than md = 2-8-2 // md and below = 12(fullscreen) -->
-    <b-row class="justify-content-center" style="background:#eee">
-      <b-col col md="2" class="d-none d-md-table-cell">1 of 3</b-col>
-      <b-col sm="12" md="8" style="background:white">
-        <nuxt/>
-      </b-col>
-      <b-col col md="2" class="d-none d-md-table-cell">3 of 3</b-col>
-    </b-row>
-
-      
+    
+    <div class="container-fluid">
+      <!-- larger than md = 2-8-2 // md and below = 12(fullscreen) -->
+      <b-row class="justify-content-center" style="background:#eee">
+        <b-col col md="2" class="d-none d-md-table-cell">1 of 3</b-col>
+        <b-col col sm="12" md="8" style="background:white">
+          <nuxt/>
+        </b-col>
+        <b-col col md="2" class="d-none d-md-table-cell">3 of 3</b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
 <style>
 .container {
   height: 100vh;
+}
+.navbar {
+  width: 100%;
 }
 </style>
