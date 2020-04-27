5beb086a9018261b6fba2a0357f561ea81b34e14
TcardLab <tcardlab@gmail.com>
2020-04-27 15:25:24 -0400

    grammar

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
 
     <p> <!-- style="word-wrap: break-word;" -->
       Braille is a different typographic representation of the alphabet for the visually impaired. 
-      Unfortunately, it has a tremendously low literacy rate and few are even aware of that (learn more). 
+      Unfortunately, it has a tremendously low literacy rate, and few are even aware of that (learn more). 
 
 
       To the uninitiated, Braille may seem bewildering and in fact, it is recognized that
@@ -33,7 +33,7 @@
        "[...] the dots are assigned in no obvious order" <font-awesome-icon icon="external-link-alt"  style="font-size: 0.75rem"/>
       </a>.
       <br><br>
-      <strong>However, Braille as I see it, is beautifully divided and organized by defining features.</strong> 
+      <strong>However, Braille as I see it is beautifully divided and organized by defining features.</strong> 
 
       The Alphabet is easily distinguishable from punctuation, end marks from internal punctuation, 
       and even small from large punctuation.
@@ -89,21 +89,21 @@
       <li> A ternary tree of depth 2, projected east/right [Easy mental traversal]. </li>
       <li> Depth corresponds to cell columns, depth 1 = Column 1 [Inherent logic]. </li>
       <li> The first 2 dots of a column are filled, "top, both, bottom" relative to branch direction [Inherent logic]. </li>
-      <li> The Lowest global branch is reserved for punctuation- with their own rules [Easy distinction]. Leaving 10 spots for alphanumerics.</li>
+      <li> The Lowest global branch is reserved for punctuation, leaving 10 spots for alphanumerics[Easy distinction].</li>
       <li> Alphanumerics are assigned top-to-bottom, left-to-right [~intuitive reading]. </li>
-      <li> Numbers are preceded with a number modifier. The alphabet is broken into sets of 10 [Rules 1-5 apply to all encodings (meta rule)] 
-                        where dots 3 and 3+6 cover 11-20 & 21-26 respectively, “W” with holding (learn why). </li> 
+      <li> Numbers are preceded with a number modifier. The alphabet is broken into sets of 10 [Rules 1-5 apply to all encodings (meta-rule)] 
+                        where dots 3 and 3+6 cover 11-20 & 21-26 respectively, “W” withholding (learn why). </li> 
     </ol>
     
 
     <p>
       While explicit instructions are often intimidating, I guarantee it is intuitive. 
-      These rules are an instructional formality, that will likely be forget once you understand the inherent organization. 
+      These rules are an instructional formality, that will likely be forgotten once you understand the inherent organization. 
       Before getting started, it's important to clarify that this is a mnemonic aid; you will be able to encode and decipher braille [G1], 
       but fluency is a matter of practice & rote memorization (not just letters, but words, groupings, contractions and ultimately G2).
     </p>
     <div style="width: 90%">
-      <h5>Step 1) Generating The Tree:</h5>
+      <h5>Step 1) Generating the Tree:</h5>
       <p>
         The structure we will be working with is a ternary tree. This means at each node in the structure,
         there are 3 branches. The tree has a depth of 2, meaning this branching process occurs twice off the root node. 
@@ -111,16 +111,16 @@
         there will be 4 such structures.
       </p>
 
-      <h5>Step 2 ) Assigning cell Columns:</h5>
+      <h5>Step 2) Assigning cell Columns:</h5>
       <p>Our first step in generating Braille is to assign cell columns by way of depth.</p>
       <ul>
-        <li>The root ( depth 0 ) has no columns</li>
-        <li>depth 1 adds the first column of dots ( 1-3)</li>
+        <li>The root (depth 0) has no columns</li>
+        <li>depth 1 adds the first column of dots (1-3)</li>
         <li>depth 2 adds the second column to the prior nodes cell.</li>
       </ul>
       <p>In this way, as you scan Braille cells one column at a time you move forward one branch in the tree.</p>
 
-      <h5>Step3 ) Assigning Dots to Columns</h5>
+      <h5>Step 3) Assigning Dots to Columns</h5>
       <p>
         As you may know, Braille is often separated into repeating sets, distinguished by the last dots of each column.
         For this reason, this structure only assigns the top 2 dots in a column. Given our rightward projection,
@@ -128,26 +128,26 @@
         and downward is just the bottom dot of the top 4. This lends itself to the saying "Top-2-Bottom".
       </p>
 
-      <h5>step 4 ) Reserve Punctuation:</h5>
+      <h5>Step 4) Reserve Punctuation:</h5>
       <p>
         The lowest global node at each depth is reserved for punctuation. This will leave to nodes left for Alphanumerics.
         This placement makes distinguishing the two very easy - as punctuation never occupy dots 1 or 4 while Alphanumerics must.
-        We wont assign punctuation just yet, as they are unsorted and require a little more effort ( click more to see ).
+        We won't assign punctuation just yet, as they are unsorted and require a little more effort (click more to see).
       </p>
 
-      <h5>Step 5 ) Assignment Order:</h5>
+      <h5>Step 5) Assignment Order:</h5>
       <p>
-        Alphanumeric sets are assigned in globally, order* from top branch toward the bottom,
-        then to the next depth[ Top to bottom, Left to right ].
+        Alphanumeric sets are assigned globally, in order from the top branch toward the bottom,
+        then to the next depth [Top-to-Bottom, Left-to-Right].
       </p>
 
-      <h5>Step 6 ) Alphanumeric Sets:</h5>
-      <p>Sets are identified either through a modifier ( numbers ) or presence of dots 3 & 6.</p>
+      <h5>Step 6) Alphanumeric Sets:</h5>
+      <p>Sets are identified either through a modifier (numbers) or the presence of dots 3 & 6.</p>
       <ul>
         <li>[A - J] Is filled in the absence of 3&6</li>
         <li>[1 - 9+0] Is identical to [A-J], but the string of #'s will be preceded by a number modifier.</li>
         <li> K-T adds dot 3 to the cells. </li>
-        <li> [U, V, X, Y, Z ] (u - Z minus w) adds dots 3&6 to each cell. Words [And, For, Of, The, With] fill the remaining spaces. They are in alphabetical order.</li>
+        <li> [U, V, X, Y, Z] (u - Z minus w) adds dots 3&6 to each cell. Words [And, For, Of, The, With] fill the remaining spaces. They are in alphabetical order.</li>
       </ul>
       
     </div>
