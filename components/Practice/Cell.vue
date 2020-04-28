21e3ce22cc3ca29d1a855b6070816630aafb0e3e
TcardLab <tcardlab@gmail.com>
2020-04-27 21:50:11 -0400

    define js events
    passing event attr to v-bind wont work.
    so its neccessary to use js.
    
    However, it may be better to pass native events from the parent.
    idk

--- components/Practice/Cell.vue
+++ components/Practice/Cell.vue
@@ -1,8 +1,6 @@
 <template>
   <svg id="cell" 
-    @mousedown="touch&&(click=true)"
-    @mouseup="touch&&(checkAnswer())" 
-    @mouseleave="touch&&(checkAnswer())"
+    ref="svg"
   >
     <rect :fill="color"/>
     <circle
@@ -13,15 +11,14 @@
       :cy="(i % 3) * 2.5 + 1.75 + 'mm'"
       r="0.75mm"
 
+      ref="circle"
       :id="touch&&(cellIndex+''+i)"
-      @mousedown="touch&&(update(cellIndex, i))"
-      @mouseover="click&&(update(cellIndex, i))"
     />
   </svg>
 </template>
 
 <!-- move touch stuff to mixin and explode with v-bind="attrs" -->
-
+<!-- v-bind="touch&&(attr.circle)" -->
 <script> 
 export default {
   props: {
@@ -43,17 +40,59 @@ export default {
   },
   data(){
     return{
-      click: false
+      click: false,
+      events: {
+        svg: {
+          mousedown: 'this.clicked()',
+          mouseup: 'this.checkAnswer()', 
+          mouseleave: "this.touch&&(thischeckAnswer())",
+        },
+        circle: {
+          mousedown: "touch&&(update(cellIndex, i))",
+          mouseover: "click&&(update(cellIndex, i))",
+        }
+      }
+    }
+  },
+  mounted() {
+    if (this.touch) {
+      const svg = this.$refs.svg
+      /* this.events.svg.forEach((k,v) => {
+        svg.addEventListener(k, ()=>eval(v))
+      }) */
+      svg.addEventListener('mousedown', ()=>this.clicked()) 
+      svg.addEventListener('mouseup', ()=>this.checkAnswer())
+      svg.addEventListener('mouseleave', ()=>this.checkAnswer())
+
+      const circleArr = this.$refs.circle
+      circleArr.forEach((c, i) => {
+        c.addEventListener('mousedown', ()=>this.update(this.cellIndex, i)) 
+        c.addEventListener('mouseover', ()=>this.click&&(this.update(this.cellIndex, i)))
+      })
+    }
+  },
+  beforeDestroy() {
+    if (this.touch) {
+      const svg = this.$refs.svg
+      svg.removeEventListener('mousedown', ()=>this.clicked()) 
+      svg.removeEventListener('mouseup', ()=>this.checkAnswer())
+      svg.removeEventListener('mouseleave', ()=>this.checkAnswer())
     }
   },
   methods: {
+    $eval (payload) {
+      return eval(payload) // eslint-disable-line
+    },
+    clicked() {
+      this.click=true
+    },
     checkAnswer() {
       this.click = false
       this.$parent.checkAnswer()
     },
     update(cellIndex, i) {
       this.$parent.updateArr(cellIndex, i)
-    }
+    },
   }
 }
 </script>
---
+++ components/Practice/CellClickMixin.js
@@ -0,0 +1,11 @@
+// define a mixin object
+var myMixin = {
+  created: function () {
+    this.hello()
+  },
+  methods: {
+    hello: function () {
+      console.log('hello from mixin!')
+    }
+  }
+}
